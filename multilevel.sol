004730e5bd54b01e3ec7b4a4711ea8e31d4feb76
Ishan Sathe <86941488+ishansathe@users.noreply.github.com>
2023-01-04 13:50:06 +0530

    Create multilevel.sol
    Program demonstrating multilevel inheritance

---
+++ multilevel.sol
@@ -0,0 +1,52 @@
+// Solidity program to demonstrate
+// Constructor in Inheritance
+pragma solidity ^0.8.0;
+
+// Creating a contract
+contract Base {
+
+// Declaring variable
+uint data;
+
+// Defining a constructor
+constructor(uint _data) public {
+	data = _data;
+}
+
+// Defining function
+function Print(
+) public returns(string memory){
+	return "Direct Initialization";
+}
+
+}
+
+// Child contract inheriting
+// the parent contract 'Base'
+contract Derived is Base(2){
+
+	// Defining a constructor
+	constructor() public {}
+
+// Defining function to access
+// variable of parent contract
+function getData() external returns(uint){
+	uint result = data ** 2;
+	return result;
+}
+}
+
+// Caller contract
+contract caller{
+
+// Creating an object of child contract
+	Derived c = new Derived();
+
+// Accessing functions of parent
+// and child contract using
+// object of child contract
+	function getResult() public returns(uint){
+		c.Print();
+		return c.getData();
+	}
+}
